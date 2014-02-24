fe778848d3a019da53f7a885dc6607b91ca904ce
Jason Fieldman <jason@fieldman.org>
2014-02-24 13:09:50 -0800

    added monster view base class

---
+++ Project Fieldlord/Classes/Models/MonsterView.h
@@ -0,0 +1,13 @@
+//
+//  MonsterView.h
+//  Project Fieldlord
+//
+//  Created by Jason Fieldman on 2/24/14.
+//  Copyright (c) 2014 Jason Fieldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MonsterView : UIView
+
+@end
---
+++ Project Fieldlord/Classes/Models/MonsterView.m
@@ -0,0 +1,31 @@
+//
+//  MonsterView.m
+//  Project Fieldlord
+//
+//  Created by Jason Fieldman on 2/24/14.
+//  Copyright (c) 2014 Jason Fieldman. All rights reserved.
+//
+
+#import "MonsterView.h"
+
+@implementation MonsterView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
