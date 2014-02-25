5246d169c9634c0438f592a3071eeffd131c656d
Jason Fieldman <jason@fieldman.org>
2014-02-25 08:39:01 -0800

    added monster info

---
+++ Project Fieldlord/Classes/Models/MonsterInfo.h
@@ -0,0 +1,13 @@
+//
+//  MonsterInfo.h
+//  Project Fieldlord
+//
+//  Created by Jason Fieldman on 2/25/14.
+//  Copyright (c) 2014 Jason Fieldman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MonsterInfo : NSObject
+
+@end
---
+++ Project Fieldlord/Classes/Models/MonsterInfo.m
@@ -0,0 +1,13 @@
+//
+//  MonsterInfo.m
+//  Project Fieldlord
+//
+//  Created by Jason Fieldman on 2/25/14.
+//  Copyright (c) 2014 Jason Fieldman. All rights reserved.
+//
+
+#import "MonsterInfo.h"
+
+@implementation MonsterInfo
+
+@end
