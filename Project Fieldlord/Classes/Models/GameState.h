9360ee67ba10da418e023e4aaf0f9c78bcfec0b5
Jason Fieldman <jason@fieldman.org>
2014-02-25 07:24:32 -0800

    added game state

---
+++ Project Fieldlord/Classes/Models/GameState.h
@@ -0,0 +1,13 @@
+//
+//  GameState.h
+//  Project Fieldlord
+//
+//  Created by Jason Fieldman on 2/25/14.
+//  Copyright (c) 2014 Jason Fieldman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameState : NSObject
+
+@end
---
+++ Project Fieldlord/Classes/Models/GameState.m
@@ -0,0 +1,13 @@
+//
+//  GameState.m
+//  Project Fieldlord
+//
+//  Created by Jason Fieldman on 2/25/14.
+//  Copyright (c) 2014 Jason Fieldman. All rights reserved.
+//
+
+#import "GameState.h"
+
+@implementation GameState
+
+@end
