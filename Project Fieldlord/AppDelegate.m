64088e49eb41c791afb29a20cd45fd79c9ccb64b
Jason Fieldman <jason@fieldman.org>
2014-03-04 18:29:21 -0800

    added flurry init

--- Project Fieldlord/AppDelegate.m
+++ Project Fieldlord/AppDelegate.m
@@ -13,6 +13,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+	/* Install flurry */
+	[Flurry setCrashReportingEnabled:YES];
+	[Flurry startSession:FLURRY_KEY];
+	
 	/* Seed random time */
 	srand(time(0));
 	InitializeMathHelper();
